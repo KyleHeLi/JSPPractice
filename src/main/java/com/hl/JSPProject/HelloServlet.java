f4a783ca7edcf11b82af60e422fb289c0c24dbfa
Kyle Lee <hli162@uottawa.ca>
2017-08-16 11:45:34 -0400

    re-organize the JSP project structure

--- src/main/java/com/hl/JSPProject/HelloServlet.java
+++ src/main/java/com/hl/JSPProject/HelloServlet.java
@@ -1,8 +1,21 @@
 package com.hl.JSPProject;
 
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
 
 public class HelloServlet extends HttpServlet {
 
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+        try {
+            response.getWriter().println("<h1>Hello Servlet!</h1");
+            response.getWriter().println(new Date().toLocaleString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
