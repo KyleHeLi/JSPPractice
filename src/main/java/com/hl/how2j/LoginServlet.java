6117245b3f7dc2ecc4e5e8825dad02eb9cc145c3
Kyle Lee <hli162@uottawa.ca>
2017-08-18 16:12:25 -0400

    add login function

---
+++ src/main/java/com/hl/how2j/LoginServlet.java
@@ -0,0 +1,35 @@
+package com.hl.how2j;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+    
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1072959192875706864L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        System.out.println("Username: " + username);
+        System.out.println("Passwrod: " + password);
+        
+        String html = null;
+        if ("admin".equals(username) && "123".equals(password)) {
+            html = "<div style=\"color:green\">success</div>";
+        } else {
+            html = "<div style=\"color:red\">failed</div>";
+        }
+        
+        PrintWriter out = response.getWriter();
+        out.println(html);
+    }
+}
